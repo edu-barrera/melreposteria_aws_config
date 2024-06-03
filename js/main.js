3c9877c4d3a65b7ba618a7e50c7c183776356b2f
edu-barrera <barreracarloseduardo03@gmail.com>
2024-06-03 17:16:39 -0600

    Added HTML and styles to form as well, just missing the validation

--- js/main.js
+++ js/main.js
@@ -39,3 +39,36 @@ window.onload = function () {
       );
     });
 };
+
+//valores de los campos
+const form = document.getElementsByTagName("form");
+const nombre = document.getElementById("name").value.trim();
+const email = document.getElementById("email").value.trim();
+const telefono = document.getElementById("telefono").value.trim();
+const message = document.getElementById("mensaje").value.trim();
+const error = document.getElementById("error");
+
+const emailRegExp =
+  /^[a-zA-Z0-9.!#$%&'+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)$/;
+
+let isValid = true;
+
+function validarForm() {
+  if (nombre === "") {
+    alert("Por favor ingrese su nombre.");
+    isValid = false;
+  }
+  if (email === "") {
+    alert("Por favor ingrese un correo válido");
+    isValid = false;
+  }
+  if (isValid) {
+    alert("Formulario enviado exitosamente!");
+  }
+}
+let btnEnviar = document
+  .getElementById("btnEnviar")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    validarForm();
+  });
