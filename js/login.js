e7dc83b45f0a1cb9326633d69a750d1ddd76f784
edu-barrera <barreracarloseduardo03@gmail.com>
2024-06-24 17:27:12 -0600

    login y log off changes

--- js/login.js
+++ js/login.js
@@ -1,116 +1,129 @@
-
-
 // mostrar alertas de bootstrap
 function mostrarAlerta(campoId, mensaje, tipo = "warning") {
-    const alertContainer = document.getElementById(campoId);
-    if (alertContainer) {
-        alertContainer.innerText = mensaje;
-        alertContainer.className = `alert alert-${tipo}`;
-        alertContainer.style.display = 'block';
-    }
+  const alertContainer = document.getElementById(campoId);
+  if (alertContainer) {
+    alertContainer.innerText = mensaje;
+    alertContainer.className = `alert alert-${tipo}`;
+    alertContainer.style.display = "block";
+  }
 }
 // ocultaar alertas de bootstrap
 function ocultarAlerta(campoId) {
-    const alertContainer = document.getElementById(campoId);
-    if (alertContainer) {
-        alertContainer.style.display = 'none';
-    }
+  const alertContainer = document.getElementById(campoId);
+  if (alertContainer) {
+    alertContainer.style.display = "none";
+  }
 }
 
 // ==============================REGISTRO =============================================//
 
 const registroForm = document.querySelector("#registroFormulario");
 registroForm.addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    // ocultar las alertas al iniciar la validación
-    ocultarAlerta("nameAlert");
-    ocultarAlerta("emailRegistroAlert");
-    ocultarAlerta("telefonoAlert");
-    ocultarAlerta("passwordRegistroAlert");
-    ocultarAlerta("passwordConfirmAlert");
-    ocultarAlerta("registroSuccessAlert");
+  // ocultar las alertas al iniciar la validación
+  ocultarAlerta("nameAlert");
+  ocultarAlerta("emailRegistroAlert");
+  ocultarAlerta("telefonoAlert");
+  ocultarAlerta("passwordRegistroAlert");
+  ocultarAlerta("passwordConfirmAlert");
+  ocultarAlerta("registroSuccessAlert");
 
-    const nombre = document.querySelector("#name").value;
-    const telefono = document.querySelector("#telefono").value;
-    const emailRegistro = document.querySelector("#emailRegistro").value;
-    const contraseñaRegistro = document.querySelector("#passwordRegistro").value;
-    const confirmContraseña = document.querySelector("#passwordConfirm").value;
+  const nombre = document.querySelector("#name").value;
+  const telefono = document.querySelector("#telefono").value;
+  const emailRegistro = document.querySelector("#emailRegistro").value;
+  const contraseñaRegistro = document.querySelector("#passwordRegistro").value;
+  const confirmContraseña = document.querySelector("#passwordConfirm").value;
 
-    // expresiones regulares y validaciones
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    const telefonoRegex = /^[1-9]\d{9}$/;
-    const contraseñaRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/; 
+  // expresiones regulares y validaciones
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  const telefonoRegex = /^[1-9]\d{9}$/;
+  const contraseñaRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 
-    let errores = false;
+  let errores = false;
 
-    if (!emailRegex.test(emailRegistro)) {
-        mostrarAlerta("emailRegistroAlert", "Email no válido");
-        errores = true;
-    }
+  if (!emailRegex.test(emailRegistro)) {
+    mostrarAlerta("emailRegistroAlert", "Email no válido");
+    errores = true;
+  }
 
-    if (!telefonoRegex.test(telefono)) {
-        mostrarAlerta("telefonoAlert", "Teléfono no válido. Debe contener 10 dígitos.");
-        errores = true;
-    }
+  if (!telefonoRegex.test(telefono)) {
+    mostrarAlerta(
+      "telefonoAlert",
+      "Teléfono no válido. Debe contener 10 dígitos."
+    );
+    errores = true;
+  }
 
-    if (!contraseñaRegex.test(contraseñaRegistro)) {
-        mostrarAlerta("passwordRegistroAlert", "Contraseña no válida. Debe contener mínimo 8 caracteres, al menos una letra y un número.");
-        errores = true;
-    }
+  if (!contraseñaRegex.test(contraseñaRegistro)) {
+    mostrarAlerta(
+      "passwordRegistroAlert",
+      "Contraseña no válida. Debe contener mínimo 8 caracteres, al menos una letra y un número."
+    );
+    errores = true;
+  }
 
-    if (contraseñaRegistro !== confirmContraseña) {
-        mostrarAlerta("passwordConfirmAlert", "Las contraseñas no coinciden");
-        errores = true;
-    }
+  if (contraseñaRegistro !== confirmContraseña) {
+    mostrarAlerta("passwordConfirmAlert", "Las contraseñas no coinciden");
+    errores = true;
+  }
 
-    if (errores) {
-        return;
-    }
+  if (errores) {
+    return;
+  }
 
-    const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
-    const usuarioRegistrado = usuarios.find(user => user.email === emailRegistro);
+  const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+  const usuarioRegistrado = usuarios.find(
+    (user) => user.email === emailRegistro
+  );
 
-    if (usuarioRegistrado) {
-        mostrarAlerta("emailRegistroAlert", "El correo ya está registrado");
-        return;
-    }
+  if (usuarioRegistrado) {
+    mostrarAlerta("emailRegistroAlert", "El correo ya está registrado");
+    return;
+  }
 
-    usuarios.push({ nombre, telefono, email: emailRegistro, contraseña: contraseñaRegistro });
-    localStorage.setItem("usuarios", JSON.stringify(usuarios));
-    mostrarAlerta("registroSuccessAlert", "Registro exitoso", "success");
-
-   
+  usuarios.push({
+    nombre,
+    telefono,
+    email: emailRegistro,
+    contraseña: contraseñaRegistro,
+  });
+  localStorage.setItem("usuarios", JSON.stringify(usuarios));
+  mostrarAlerta("registroSuccessAlert", "Registro exitoso", "success");
 });
 
 // =========================================== LOGIN ============================================/////
 const loginForm = document.querySelector("#loginForm");
 loginForm.addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    // ocultar alertas al iniciar la validación
+  // ocultar alertas al iniciar la validación
+  ocultarAlerta("emailLoginAlert");
+  ocultarAlerta("passwordLoginAlert");
+
+  const email = document.querySelector("#emailLogin").value;
+  const contraseña = document.querySelector("#passwordLogin").value;
+  const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
+
+  const validarUsuario = usuarios.find(
+    (user) => user.email === email && user.contraseña === contraseña
+  );
+
+  if (!validarUsuario) {
+    mostrarAlerta("emailLoginAlert", "Correo y/o contraseña incorrectos");
+    return;
+  }
+
+  mostrarAlerta(
+    "emailLoginAlert",
+    `Bienvenido ${validarUsuario.nombre}`,
+    "success"
+  );
+
+  // redireccionar a la pagina de index despues de algunos segundos
+  setTimeout(() => {
     ocultarAlerta("emailLoginAlert");
-    ocultarAlerta("passwordLoginAlert");
-
-    const email = document.querySelector("#emailLogin").value;
-    const contraseña = document.querySelector("#passwordLogin").value;
-    const usuarios = JSON.parse(localStorage.getItem("usuarios")) || [];
-
-    const validarUsuario = usuarios.find(user => user.email === email && user.contraseña === contraseña);
-
-    if (!validarUsuario) {
-        mostrarAlerta("emailLoginAlert", "Correo y/o contraseña incorrectos");
-        return;
-    }
-
-    mostrarAlerta("emailLoginAlert", `Bienvenido ${validarUsuario.nombre}`, "success");
-
-    // redireccionar a la pagina de index despues de algunos segundos
-    setTimeout(() => {
-        ocultarAlerta("emailLoginAlert");
-    // redireccionar a la página principal 
-    window.location.href = 'index.html';
-        
-    }, 2000);
-    
+    // redireccionar a la página principal
+    window.location.href = "index.html";
+  }, 2000);
 });
--- js/sections.js
+++ js/sections.js
@@ -1,12 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const nav = document.querySelector("#navbar-example2");
-  const footer = document.querySelector("#footer");
-  const nav2 = document.querySelector("#navbar-header");
+const nav = document.querySelector("#navbar-example2");
+const footer = document.querySelector("#footer");
+const nav2 = document.querySelector("#navbar-header");
 
+document.addEventListener("DOMContentLoaded", function () {
   /* INSERCION DE LA SECCION HEADER  MEDIANTE  CODIGO JS Y LA FUNCION insertAdjacentHTML */
   try {
-      nav2.insertAdjacentHTML(
-          "beforeend", `
+    nav2.insertAdjacentHTML(
+      "beforeend",
+      `
        <div>
           <nav class="navbar fixed-top navbar-expand-sm bg-body-tertiary"> 
               <div class="container-fluid">
@@ -51,7 +52,7 @@ document.addEventListener("DOMContentLoaded", function () {
                                           <img src="./src/Iconos/usuario.png" alt="usuario">
                                       </a>
                                   </li>
-                            <ul class="navbar-nav flew-row flex-wrap navbar-icons">
+                            <ul class="navbar-nav flew-row flex-wrap navbar-icons" id ="login-icons">
                                   <li class="nav-item">
                                      <a class="nav-link" href="carrito.html"> 
                                           <img src="./src/Iconos/bolsa.png" alt="carrito">
@@ -63,55 +64,65 @@ document.addEventListener("DOMContentLoaded", function () {
           </nav>
       </div>  
   `
-      );
+    );
   } catch (ex) {
-      console.log("ocurrio un error al insertar el html " + ex);
+    console.log("ocurrio un error al insertar el html " + ex);
   }
 
   /* INSERCION DE LA SECCION FOOTER  MEDIANTE  COIGO JS Y LA FUNCION insertAdjacentHTML */
   footer.insertAdjacentHTML(
-      "beforeend",
-      `
-    <div class="barra">
-      <a class="logo" href="#">
-        <img src="./src/Iconos/oso.png" alt="logo mel reposteria" />
-      </a>
-
-      <nav class="navegacion">
-        <div class="seccion conocenos">
-          <h3>Conócenos</h3>
-          <a href="./acerca.html">Acerca de nosotros</a>
-        </div>
-
-        <div class="seccion redes-sociales">
-          <h3>Redes sociales</h3>
-          <a href="https://www.instagram.com/melreposteriamx/">Instagram</a>
-          <a href="https://www.facebook.com/profile.php?id=100067550652660">Facebook</a>
-        </div>
-
-        <div class="seccion cursos">
-          <h3>Cursos</h3>
-          <a href="#">Decoración de flores</a>
-        </div>
-
-        <div class="seccion contacto">
-          <h3>Contacto</h3>
-          <a href="./contactanos.html"> Contáctanos</a>
-          <a href="./contactanos.html">Cotizaciones</a>
-        </div>
-
-        <div class="seccion mi-cuenta">
-          <h3>Mi cuenta</h3>
-          <a href="#">Iniciar sesión</a>
-          <a href="#">Registrarse</a>
-        </div>
-
-        <div class="seccion ubicacion">
-          <h3>Ubicación</h3>
-          <a href="#">Pachuca</a>
-        </div>
-      </nav>
-    </div>
-    `
+    "beforeend",
+    `   <footer class="d-flex flex-wrap justify-content-between align-items-center py-3">
+  <div class="col-md-4 d-flex align-items-center">
+  <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+  <a class="footer-brand" href="#">
+     <img src="./src/Iconos/oso.png" class="logo">
+    
+  </a>
+  <span class="mb-3 mb-md-0 text-muted">Elaborado por @cookiecoding</span>
+</div>
+<ul class="nav col-md-4 justify-content-center list-unstyled">
+  <li class="ms-3">
+    <a class="text-muted" href="https://www.instagram.com/melreposteriamx/">
+      <i class="bi bi-instagram"></i>
+    </a>
+  </li>
+  <li class="ms-3">
+    <a class="text-muted" href="https://www.facebook.com/profile.php?id=100067550652660">
+      <i class="bi bi-facebook"></i>
+  `
   );
+  // Consiguiendo nombre
+  const llave = "usuarios";
+  const valor = localStorage.getItem(llave);
+  const registro = JSON.parse(valor);
+  const nombreUsuario = registro[0].nombre;
+  const logIcons = document.getElementById("login-icons");
+  const divNuevo = document.createElement("div");
+  if (valor) {
+    divNuevo.innerHTML = `<div class="bienvenidoSeccion"> <p class="bienvenidosP">Bienvenido ${nombreUsuario} 🐻</p> 
+        <button type="button" class="btn btn-danger" id="cerrarSesionBtn">
+          Cerrar sesión
+        </button>
+        </div>
+     `;
+    logIcons.appendChild(divNuevo);
+  } else {
+    console.log("No hay sesión iniciada");
+  }
+
+  function deleteData() {
+    localStorage.removeItem("usuarios");
+  }
+
+  //////Cerrar sesión//////////
+
+  cerrarSesionBtn.addEventListener("click", () => {
+    divNuevo.innerHTML = `Nos vemos pronto ${nombreUsuario}!`;
+    deleteData();
+    setTimeout(() => {
+      // redireccionar a la página principal
+      window.location.href = "index.html";
+    }, 3000);
+  });
 });
