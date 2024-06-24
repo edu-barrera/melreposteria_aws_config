795448a6c3d34cb3501071505ecbc4438963741d
edu-barrera <barreracarloseduardo03@gmail.com>
2024-06-24 17:10:01 -0600

    Update sections.js
    Agregué sección en la que muestra al usuario su nombre al tener su sesión abiera y botón con funcionalidad para cerrar sesión

--- js/sections.js
+++ js/sections.js
@@ -1,12 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const nav = document.querySelector("#navbar-example2");
-  const footer = document.querySelector("#footer");
-  const nav2 = document.querySelector("#navbar-header");
+const nav = document.querySelector("#navbar-example2");
+const footer = document.querySelector("#footer");
+const nav2 = document.querySelector("#navbar-header");
 
+document.addEventListener("DOMContentLoaded", function () {
   /* INSERCION DE LA SECCION HEADER  MEDIANTE  CODIGO JS Y LA FUNCION insertAdjacentHTML */
   try {
-      nav2.insertAdjacentHTML(
-          "beforeend", `
+    nav2.insertAdjacentHTML(
+      "beforeend",
+      `
        <div>
           <nav class="navbar fixed-top navbar-expand-sm bg-body-tertiary"> 
               <div class="container-fluid">
@@ -51,7 +52,7 @@ document.addEventListener("DOMContentLoaded", function () {
                                           <img src="./src/Iconos/usuario.png" alt="usuario">
                                       </a>
                                   </li>
-                            <ul class="navbar-nav flew-row flex-wrap navbar-icons">
+                            <ul class="navbar-nav flew-row flex-wrap navbar-icons" id ="login-icons">
                                   <li class="nav-item">
                                      <a class="nav-link" href="carrito.html"> 
                                           <img src="./src/Iconos/bolsa.png" alt="carrito">
@@ -63,34 +64,62 @@ document.addEventListener("DOMContentLoaded", function () {
           </nav>
       </div>  
   `
-      );
+    );
   } catch (ex) {
-      console.log("ocurrio un error al insertar el html " + ex);
+    console.log("ocurrio un error al insertar el html " + ex);
   }
 
   /* INSERCION DE LA SECCION FOOTER  MEDIANTE  COIGO JS Y LA FUNCION insertAdjacentHTML */
   footer.insertAdjacentHTML(
-      "beforeend",
-      `   <footer class="d-flex flex-wrap justify-content-between align-items-center py-3">
-    <div class="col-md-4 d-flex align-items-center">
-    <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
-    <a class="footer-brand" href="#">
-       <img src="./src/Iconos/oso.png" class="logo">
-      
+    "beforeend",
+    `   <footer class="d-flex flex-wrap justify-content-between align-items-center py-3">
+  <div class="col-md-4 d-flex align-items-center">
+  <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+  <a class="footer-brand" href="#">
+     <img src="./src/Iconos/oso.png" class="logo">
+    
+  </a>
+  <span class="mb-3 mb-md-0 text-muted">Elaborado por @cookiecoding</span>
+</div>
+<ul class="nav col-md-4 justify-content-center list-unstyled">
+  <li class="ms-3">
+    <a class="text-muted" href="https://www.instagram.com/melreposteriamx/">
+      <i class="bi bi-instagram"></i>
     </a>
-    <span class="mb-3 mb-md-0 text-muted">Elaborado por @cookiecoding</span>
-  </div>
-
-  <ul class="nav col-md-4 justify-content-center list-unstyled">
-    <li class="ms-3">
-      <a class="text-muted" href="https://www.instagram.com/melreposteriamx/">
-        <i class="bi bi-instagram"></i>
-      </a>
-    </li>
-    <li class="ms-3">
-      <a class="text-muted" href="https://www.facebook.com/profile.php?id=100067550652660">
-        <i class="bi bi-facebook"></i>
-
-    `
+  </li>
+  <li class="ms-3">
+    <a class="text-muted" href="https://www.facebook.com/profile.php?id=100067550652660">
+      <i class="bi bi-facebook"></i>
+  `
   );
+  // Consiguiendo nombre
+  const llave = "usuarios";
+  const valor = localStorage.getItem(llave);
+  const registro = JSON.parse(valor);
+  const nombreUsuario = registro[0].nombre;
+  const logIcons = document.getElementById("login-icons");
+  const divNuevo = document.createElement("div");
+  if (valor) {
+    divNuevo.innerHTML = `<div class="bienvenidoSeccion"> <p class="bienvenidosP">Bienvenido ${nombreUsuario} 🐻</p> 
+        <button type="button" class="btn btn-danger" id="cerrarSesionBtn">
+          Cerrar sesión
+        </button>
+        </div>
+     `;
+    logIcons.appendChild(divNuevo);
+  } 
+  function deleteData() {
+    localStorage.removeItem("usuarios");
+  }
+
+  //////Cerrar sesión//////////
+
+  cerrarSesionBtn.addEventListener("click", () => {
+    divNuevo.innerHTML = `Nos vemos pronto ${nombreUsuario}!`;
+    deleteData();
+    setTimeout(() => {
+      // redireccionar a la página principal
+      window.location.href = "index.html";
+    }, 3000);
+  });
 });
