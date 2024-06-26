f80480531c7aab963b2a8704efa3346c638d6452
MelanieDCR <126915136+MelanieDCR@users.noreply.github.com>
2024-06-25 22:36:57 -0600

    Modifique precio

--- js/products.js
+++ js/products.js
@@ -71,21 +71,21 @@ function addEtiquetaItem(item) {
 
 // Lista de productos
 const products = [
-    { name: 'Decoración estilo Acuarela', img: 'src/productos/acuarela3.jpg', description: 'Sumérgete en el emocionante universo de la decoración, donde aprenderás a transformar tus creaciones en auténticas obras de arte comestibles.', link: 'curso1.html', carrito: 'carrito.html' },
-    { name: 'Decoración de Personajes', img: 'src/productos/bobesponjaHalloween.jpg', description: 'Aprende a decorar deliciosos postres con el divertido y colorido mundo de Bob Esponja y sus amigos.', link: 'curso2.html', carrito: 'carrito.html' },
-    { name: 'Decoración de Catrinas', img: 'src/productos/catrina2.jpg', description: 'Descubre la tradición mexicana mientras aprendes a crear hermosas decoraciones inspiradas en esta emblemática figura del Día de los Muertos.', link: 'curso3.html', carrito: 'carrito.html' },
-    { name: 'Decoración de Flores', img: 'src/productos/flores.jpg', description: 'Aprende el apasionante arte de la decoración donde crearás exquisitas flores de azúcar y otros adornos florales para embellecer tus dulces  creaciones y sorprender a tus seres queridos. ', link: 'curso4.html', carrito: 'carrito.html' },
+    { name: 'Decoración estilo Acuarela', img: 'src/productos/acuarela3.jpg', description: 'Sumérgete en el emocionante universo de la decoración, donde aprenderás a transformar tus creaciones en auténticas obras de arte comestibles.',precio: '250', link: 'curso1.html', carrito: 'carrito.html' },
+    { name: 'Decoración de Personajes', img: 'src/productos/bobesponjaHalloween.jpg', description: 'Aprende a decorar deliciosos postres con el divertido y colorido mundo de Bob Esponja y sus amigos.',precio: '250', link: 'curso2.html', carrito: 'carrito.html' },
+    { name: 'Decoración de Catrinas', img: 'src/productos/catrina2.jpg', description: 'Descubre la tradición mexicana mientras aprendes a crear hermosas decoraciones inspiradas en esta emblemática figura del Día de los Muertos.',precio: '250', link: 'curso3.html', carrito: 'carrito.html' },
+    { name: 'Decoración de Flores', img: 'src/productos/flores.jpg', description: 'Aprende el apasionante arte de la decoración donde crearás exquisitas flores de azúcar y otros adornos florales para embellecer tus dulces  creaciones y sorprender a tus seres queridos. ',precio: '250', link: 'curso4.html', carrito: 'carrito.html' },
 
 ];
 
 // Lista de etiquetas
 const etiquetas = [
-    { name: 'Navidad', img: 'src/productos/navidad.jpg', description: 'Archivo PDF Con 5 diseños diferentes de temporada </br> Incluye diferentes medidas para etiquetas.', link: '#', carrito: 'carrito.html' },
-    { name: 'Día de Muertos', img: 'src/productos/dia de muertos.jpg', description: 'Archivo PDF Con 5 diseños diferentes de temporada </br> Incluye diferentes medidas para etiquetas.', link: '#', carrito: 'carrito.html' },
-    { name: 'San Valetín', img: 'src/productos/sanvalentin (2).jpg', description: 'Archivo PDF Con 5 diseños diferentes de temporada </br> Incluye diferentes medidas para etiquetas.', link: '#', carrito: 'carrito.html' },
-    { name: 'Halloween', img: 'src/productos/halloween.jpg', description: 'Archivo PDF Con 5 diseños diferentes de temporada </br> Incluye diferentes medidas para etiquetas.', link: '#', carrito: 'carrito.html' },
-    { name: '15 de Septiembre', img: 'src/productos/septiembre.png', description: 'Archivo PDF Con 5 diseños diferentes de temporada </br> Incluye diferentes medidas para etiquetas.', link: '#', carrito: 'carrito.html' },
-    { name: 'Día de las madres', img: 'src/productos/mothers.jpg', description: 'Archivo PDF Con 5 diseños diferentes de temporada </br> Incluye diferentes medidas para etiquetas.', link: '#', carrito: 'carrito.html' },
+    { name: 'Navidad', img: 'src/productos/navidad.jpg', description: 'Archivo PDF Con 5 diseños diferentes de temporada </br> Incluye diferentes medidas para etiquetas.',precio: '50', link: '#', carrito: 'carrito.html' },
+    { name: 'Día de Muertos', img: 'src/productos/dia de muertos.jpg', description: 'Archivo PDF Con 5 diseños diferentes de temporada </br> Incluye diferentes medidas para etiquetas.',precio: '50', link: '#', carrito: 'carrito.html' },
+    { name: 'San Valetín', img: 'src/productos/sanvalentin (2).jpg', description: 'Archivo PDF Con 5 diseños diferentes de temporada </br> Incluye diferentes medidas para etiquetas.',precio: '50', link: '#', carrito: 'carrito.html' },
+    { name: 'Halloween', img: 'src/productos/halloween.jpg', description: 'Archivo PDF Con 5 diseños diferentes de temporada </br> Incluye diferentes medidas para etiquetas.',precio: '50', link: '#', carrito: 'carrito.html' },
+    { name: '15 de Septiembre', img: 'src/productos/septiembre.png', description: 'Archivo PDF Con 5 diseños diferentes de temporada </br> Incluye diferentes medidas para etiquetas.',precio: '50', link: '#', carrito: 'carrito.html' },
+    { name: 'Día de las madres', img: 'src/productos/mothers.jpg', description: 'Archivo PDF Con 5 diseños diferentes de temporada </br> Incluye diferentes medidas para etiquetas.',precio: '50', link: '#', carrito: 'carrito.html' },
 
 ];
 
